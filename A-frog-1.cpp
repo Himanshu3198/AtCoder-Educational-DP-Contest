b21d851398c68ccdad6ecd8d8493688830874be8
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-11-02 15:37:49 +0530

    Add files via upload

---
+++ A-frog-1.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ 
+int main(){
+ 
+          int n;
+          cin>>n;
+     
+          int a[n];
+  
+           for(int i=0;i<n;i++){
+                cin>>a[i]; 
+           }
+  
+           vector<int>dp(n,0);
+  
+           
+           dp[1]=abs(a[1]-a[0]);
+       
+  
+      for(int i=2;i<n;i++){
+           
+            dp[i]=min(dp[i-1]+abs(a[i]-a[i-1]),dp[i-2]+abs(a[i]-a[i-2]));
+      }
+
+       
+       
+
+        cout<<dp[n-1]<<"\n";
+       
+  
+         return 0;
+       
+}
